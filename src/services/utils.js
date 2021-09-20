51afb721caf41c8a820c34b1eb2c5bcd34f17809
l4z0-space <lazaronshyta@gmail.com>
2021-09-20 23:48:22 +0200

    Push to heroku

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,8 +1,8 @@
 const path = {
     dev: 'http://localhost:5000/api',
-    // prod: 'https://rapihire.com/api/'
+    prod: 'https://jobmanagement12.herokuapp.com/api'
 }
 
-const base_url = path.dev
+const base_url = path.prod
 
 export default base_url
